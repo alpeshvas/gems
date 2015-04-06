48a3736bb5480e36aaa417531f98214f7a01e708
alpeshvas <alpeshvash@gmail.com>
2015-04-06 17:03:20 +0530

    Update urls.py

--- mainSite/urls.py
+++ mainSite/urls.py
@@ -1,1 +1,7 @@
+from django.conf.urls import patterns, url
+from mainSite import views
 
+urlpatterns = patterns('',
+        url(r'^$', views.index, name='index'),
+        url(r'^login/$', views.user_login, name='login1')
+ )
