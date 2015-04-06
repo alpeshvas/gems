d2b72f1fc853e3b2f9a7bfa491d054928c6aa2fd
alpeshvas <alpeshvash@gmail.com>
2015-04-06 17:04:16 +0530

    candidatelogin

--- mainSite/views.py
+++ mainSite/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+
+def user_login(request):                      #url for login page is home/login
+    if request.method == 'POST':
+    	username= request.POST.get('username')
+    	password = request.POST.get('password')
+    	user = authenticate(username=username,password=password)
+    	if user:
+    	    if (user.is_active and user.is_staff):
+    	        login(request, user)
+    		return HttpResponseRedirect('/Home/')
+    	    else:
+    		return HttpResponseRedirect('/Home/login/')
+    	else:
+    	    return HttpResponse("your account is diabled")		
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render(request, 'login.html', {})		
+def index(request):
+    context_dict = {'boldmessage': "Welcome to the GEMS"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'index.html', context_dict)
 
 # Create your views here.
