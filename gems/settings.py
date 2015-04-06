ad6ed6eba62784a432f961b22168204c1f445a49
alpeshvas <alpeshvash@gmail.com>
2015-04-06 17:23:08 +0530

    change directory for template

--- gems/settings.py
+++ gems/settings.py
@@ -13,7 +13,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 #XYZNOTE
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, "mainSite/templates"),)
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, "mainSite"),)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
